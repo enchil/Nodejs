786cf5644bb625ccf513b77ab4098d42344ca034
Enchi <enchiliang@gmail.com>
2022-10-06 15:18:20 +0800

    add module

---
+++ src/Person.mjs
@@ -0,0 +1,23 @@
+export default class Person {
+    gender = 'female';
+    constructor(name = 'vicky', age = 20) {
+        this.name = name;
+        this.age = age;
+    }
+    toJSON() {
+        const { name, age, gender } = this;
+        return { name, age, gender };
+    }
+    toString() {
+        return JSON.stringify(this);
+    }
+}
+
+//const p1 = new Person('wayne', '30');
+
+//console.log(p1 + '');
+
+export const a = 10;
+const f = n => n * n;
+
+export { f };
